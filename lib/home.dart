76826b3fc05b694d7332c3a6773a051163ee71ac
ankeetkarki <ankeetkarki30@gmail.com>
2020-11-22 10:20:02 +0545

    Initial setup

---
+++ lib/home.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Search for recipes"),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                TextField(
+                  controller: _textController,
+                ),
+                IconButton(icon: Icon(Icons.search), onPressed: () {})
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
